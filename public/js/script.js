b91da05c989e5d9a7ba5cf4c84bc296c0c127945
Ian Harris <ianmharris93@gmail.com>
2022-09-16 15:00:38 -0700

    finished process section, added queries for responsiveness, set up layout for treatment section

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,18 @@ let callNum = () =>{
     
 }
 number.addEventListener("click", callNum )
+
+// let slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setInterval(showSlides, 2000); // Change image every 2 seconds
+// }
