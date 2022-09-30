13ef7a84608840accc00cdb43eab0460e8f20bed
Ian Harris <ianmharris93@gmail.com>
2022-09-29 17:23:08 -0700

    kritter catchers finishign tocuhes

---
+++ server.js
@@ -0,0 +1,23 @@
+const PORT = 3000;
+
+const express = require("express")
+
+const http = require("http")
+const path = require("path")
+
+const app = express();
+
+// app.set("view engine", "ejs");
+
+app.use(express.static(__dirname+ "/public/"))
+// app.use("/css", express.static(path.dirname + "public/css/"));
+// app.use("/js", express.static(path.dirname + "public/js/"));
+// app.use("/img", express.static(path.dirname + "public/img/"));
+
+app.get("/", (req, res) => {
+  res.render("index.html");
+});
+
+
+
+http.createServer(app).listen(process.env.PORT || PORT, console.log(` listening on port http://localhost:${PORT}`));
